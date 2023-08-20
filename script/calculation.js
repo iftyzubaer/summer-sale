750545393f133e92cd3fdfba988e7e5a7ca02998
iftyzubaer <iftyzubaer1304.com>
2023-08-20 12:52:30 +0600

    added calculation script

---
+++ script/calculation.js
@@ -0,0 +1,71 @@
+document.getElementById('prod1').addEventListener('click', function(){
+    title=title1.innerHTML
+    price=price1.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+document.getElementById('prod2').addEventListener('click', function(){
+    title=title2.innerHTML
+    price=price2.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+document.getElementById('prod3').addEventListener('click', function(){
+    title=title3.innerHTML
+    price=price3.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+document.getElementById('prod4').addEventListener('click', function(){
+    title=title4.innerHTML
+    price=price4.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+document.getElementById('prod5').addEventListener('click', function(){
+    title=title5.innerHTML
+    price=price5.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+document.getElementById('prod6').addEventListener('click', function(){
+    title=title6.innerHTML
+    price=price6.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+document.getElementById('prod7').addEventListener('click', function(){
+    title=title7.innerHTML
+    price=price7.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+document.getElementById('prod8').addEventListener('click', function(){
+    title=title8.innerHTML
+    price=price8.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+document.getElementById('prod9').addEventListener('click', function(){
+    title=title9.innerHTML
+    price=price9.innerHTML
+    addToCart(title);
+    addPrice(price);
+});
+
+function addToCart(title) {
+    const storageBox = document.getElementById('storage-box');
+    const p = document.createElement('p');
+    const count = storageBox.childElementCount;
+    p.innerHTML = `${count+1}. ${title}`;
+    storageBox.appendChild(p);
+}
+
+function addPrice(productPrice) {
+    maxStringElement = document.getElementById('totalPrice');
+    maxString = maxStringElement.innerHTML;
+    max = parseFloat(maxString);
+    productPrice = parseFloat(productPrice);
+    totalPrice = max + productPrice;
+    maxStringElement.innerHTML = totalPrice;
+}
